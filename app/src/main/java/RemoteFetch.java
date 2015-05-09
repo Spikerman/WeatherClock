3883c06a51e86350e0607f7ebbc11deb5cb05413
Spikerman <spikechenhao@163.com>
2015-05-09 23:38:17 +0800

    Initial commit

---
+++ app/src/main/java/RemoteFetch.java
@@ -0,0 +1,12 @@
+/**
+ * Created by chenhao on 15/5/9.
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+
+public class RemoteFetch {
+
+}
